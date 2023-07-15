58f7c7a947259895cebfa348cc1a0bd18b1b2626
Akhil Nandyala <akhilpie3@gmail.com>
2023-07-15 14:43:47 +0530

    added scroll up

--- Movies page/movie.js
+++ Movies page/movie.js
@@ -87,3 +87,23 @@ window.addEventListener('click', (event) => {
         searchList.classList.add('hide-search-list');
     }
 });
+
+
+// scroll to top
+// Get the button element
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
