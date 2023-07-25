d8f9a526b94e76a9cfb22b8c4d305b2ac7b16e36
Rishab V <rishabvelvadapu@gmail.com>
2023-07-25 09:18:53 +0530

    changed borders and added current date

--- News Page/news.js
+++ News Page/news.js
@@ -16,3 +16,19 @@ function scrollToTop() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 }
+
+function getFormattedDate() {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const currentDate = new Date();
+  const dayOfWeek = daysOfWeek[currentDate.getDay()];
+  const date = currentDate.getDate();
+  const month = monthsOfYear[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+
+  return `${dayOfWeek}, ${date} ${month}, ${year}`;
+}
+
+// Set the content of the <h1> element to display the formatted date
+document.getElementById('dayAndDate').innerText = getFormattedDate();
